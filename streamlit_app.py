1dd932695c7832d444ef62587d537d1048057db3
Ross <propagatealpha@gmail.com>
2023-10-26 16:35:53 -0500

    minor edits to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,11 @@ import streamlit as st
 from PIL import Image
 from simulation import Simulation
 from trading_strategies import TradingStrategy
+import markdown
+
+
+def convert_to_markdown(text):
+    return markdown.markdown(text)
 
 
 def run():
@@ -122,6 +127,7 @@ def run():
                              acct_rules=account_rule_presets[selected_acct_preset], acct_fees=fees)
             sim.run()
             result = sim.sim_results()
+            result = convert_to_markdown(result)
             status_message.markdown(result)
         else:
             st.warning("Please provide all input values before computing.")
