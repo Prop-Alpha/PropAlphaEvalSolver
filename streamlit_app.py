47b668a0e847880fe929098452f7a98d897cdaf4
vespatrades <vespatrades@nosuchthing.org>
2023-10-26 21:20:03 -0500

    Clarify statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,12 @@ def run():
     st.title("Prop Alpha Eval Solver")
 
     st.markdown("""
-        Estimate expected value of a full payout in prop accounts with EOD trailing drawdown \n
-        given a single setup with a defined bracket and known win percentage and average mfe.\n
-        NOT FINANCIAL ADVICE. \n
-        DO YOUR OWN RESEARCH. \n
-        NO GUARANTEE OUR MATH IS CORRECT. \n
+        Estimate the expected full payout value for prop trading accounts, given a 
+        set of rules, fees, and a trade strategy. Factors in EOD trailing drawdown. 
+        Strategy includes defined order bracket, win rate, and average MFE.\n
+        NOT FINANCIAL ADVICE.\n
+        DO YOUR OWN RESEARCH.\n
+        NO GUARANTEE OUR MATH IS CORRECT.\n
         RISK DISCLAIMER: https://www.prop-alpha.com/disclaimer
     """)
 
